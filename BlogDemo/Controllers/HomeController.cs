237058dc67859fc7475cb56af0da545728d20b49
ysd322 <578402809@qq.com>
2018-10-30 10:30:41 +0800

    First commit

---
+++ BlogDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogDemo.Models;
+
+namespace BlogDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        BlogEntities db = new BlogEntities();
+        public ActionResult Index()
+        {
+            ViewBag.tuijian = db.View_Gallery
+                .Where(g => g.PosId==1)
+                .OrderByDescending(g => g.Weight)
+                .Take(10)
+                .ToList();
+            return View();
+        }
+    }
+}
